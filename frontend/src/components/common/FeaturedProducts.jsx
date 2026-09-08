016b7911c7543627746c04a526169739719bebb9
basudevkumer <codebyjhulon.bd@gmail.com>
2026-09-09 01:56:12 +0600

    feat: feature.jsx changes typo

--- frontend/src/components/common/FeaturedProducts.jsx
+++ frontend/src/components/common/FeaturedProducts.jsx
@@ -14,15 +14,15 @@ const FeaturedProducts = ({ imgSrc, imgAlt, price, category }) => {
         className="w-full h-full object-cover"
       />
       <div className="absolute bottom-0 left-0 right-0 w-full h-0 bg-red/80 group-hover:h-full transition-all duration-700 ease-in-out"></div>
-      <div className="absolute  bottom-10 left-10">
-        <p className="texts_14_regular text-[#c22b26] group-hover:text-white transition-colors duration-700">
+      <div className="absolute bottom-10 left-10">
+        <p className="texts_14_regular text-head group-hover:text-white transition-colors duration-700">
           {price}
         </p>
-        <h3 className="head_26_medium text-[#c22b26] group-hover:text-white transition-colors duration-700 pb-1.25">
+        <h3 className="head_26_medium text-head group-hover:text-white transition-colors duration-700 pb-1.25">
           {category}
         </h3>
         <Button
-          className="hover:after:w-12.5 group-hover:after:bg-white group-hover:text-white transition-colors duration-700 !text-[#c22b26]"
+          className="hover:after:w-12.5 group-hover:after:bg-white group-hover:text-white transition-colors duration-700"
           btnText="SHOP NOW"
         />
       </div>
